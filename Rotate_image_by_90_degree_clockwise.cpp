4fbe504584c7c21887478614d885c18a87eb57d9
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-27 20:46:27 +0530

    Add files via upload
    27th February 2023

---
+++ Rotate_image_by_90_degree_clockwise.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void rotate(vector < vector < int >> & matrix) {
+  int n = matrix.size();
+
+  //transposing the matrix
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      swap(matrix[i][j], matrix[j][i]);
+    }
+  }
+
+  //reversing each row of the matrix
+  for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+  }
+}
+
+int main() {
+
+  vector < vector < int >> arr;
+  arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+  cout<<"Before Rotation "<<endl;
+
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+  rotate(arr);
+
+  cout <<endl<< "After Rotation " << endl;
+
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
